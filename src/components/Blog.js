0e60758bff38a007c97a26f720a90f912d274a89
Mena Azir <AzirM6513@users.noreply.github.com>
2021-04-09 14:02:48 -0400

    added state to show more details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <li>
-    {blog.title} {blog.author}
-  </li>
-);
+import React, { useState } from 'react';
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(true);
+
+  if (showDetails) {
+    return (
+      <ul className='margin-bottom slim-black-border ul-padding'>
+        <li>
+          {blog.title} {blog.author}
+        </li>
+        <li>{blog.url}</li>
+        <li>
+          likes {blog.likes}
+          <button>like</button>
+        </li>
+        <li>{blog.user}</li>
+      </ul>
+    );
+  }
+
+  return (
+    <li>
+      {blog.title} {blog.author}
+    </li>
+  );
+};
 
 export default Blog;
