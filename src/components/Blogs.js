c84d8d6f8ed58d1675f1d44baf4ef9c122d9686e
Mena Azir <AzirM6513@users.noreply.github.com>
2021-04-09 19:48:58 -0400

    add update blog function

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -4,7 +4,14 @@ import Blog from './Blog';
 import BlogForm from './BlogForm';
 import Notification from './Notification';
 
-const Blogs = ({ blogs, user, handleLogout, errorMessage, addBlog }) => {
+const Blogs = ({
+  blogs,
+  user,
+  handleLogout,
+  errorMessage,
+  addBlog,
+  updateBlog,
+}) => {
   return (
     <div>
       <h2>Blogs</h2>
@@ -14,7 +21,7 @@ const Blogs = ({ blogs, user, handleLogout, errorMessage, addBlog }) => {
       <BlogForm createBlog={addBlog} />
       <ul>
         {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
         ))}
       </ul>
     </div>
