dbe9c720c65800107722d73e736de04272e75a52
Mena Azir <AzirM6513@users.noreply.github.com>
2021-04-09 14:09:48 -0400

    added BlogForms above blog list

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,14 +1,17 @@
 import React from 'react';
+
 import Blog from './Blog';
+import BlogForm from './BlogForm';
 import Notification from './Notification';
 
-const Blogs = ({ blogs, user, handleLogout, errorMessage }) => {
+const Blogs = ({ blogs, user, handleLogout, errorMessage, addBlog }) => {
   return (
     <div>
       <h2>Blogs</h2>
       <Notification message={errorMessage} />
       <p className='username-p'>{user.name} logged-in</p>
       <button onClick={handleLogout}>logout</button>
+      <BlogForm createBlog={addBlog} />
       <ul>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
