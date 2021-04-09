fb48b10059eac430a25d587c69cf60453979fa33
Mena Azir <AzirM6513@users.noreply.github.com>
2021-04-09 19:17:01 -0400

    made program easier to read by making a logical flow

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,43 +3,38 @@ import userService from '../services/users';
 
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false);
+  const [buttonLabel, setButtonLabel] = useState('view');
+
+  const showWhenVisible = { display: showDetails ? '' : 'none' };
 
   let username = null;
 
   const toggleDetails = () => {
     setShowDetails(!showDetails);
+    setButtonLabel(showDetails ? 'view' : 'hide');
   };
 
   if (blog.hasOwnProperty('user')) {
     username = userService.getUserById(blog.user).username;
   }
 
-  if (showDetails) {
-    return (
-      <ul className='margin-bottom slim-black-border ul-padding'>
-        <li>
-          {blog.title} {blog.author}
-          <button className='margin-left' onClick={toggleDetails}>
-            hide
-          </button>
-        </li>
-        <li>{blog.url}</li>
-        <li>
-          likes {blog.likes}
-          <button className='margin-left'>like</button>
-        </li>
-        <li>{username || blog.author}</li>
-      </ul>
-    );
-  }
-
   return (
-    <li>
-      {blog.title} {blog.author}
-      <button className='margin-left' onClick={toggleDetails}>
-        show
-      </button>
-    </li>
+    <ul className='margin-bottom slim-black-border ul-padding'>
+      <li>
+        {blog.title} {blog.author}
+        <button className='margin-left' onClick={toggleDetails}>
+          {buttonLabel}
+        </button>
+      </li>
+      <li style={showWhenVisible}>{blog.url}</li>
+      <li style={showWhenVisible}>
+        likes {blog.likes}
+        <button className='margin-left' style={showWhenVisible}>
+          like
+        </button>
+      </li>
+      <li style={showWhenVisible}>{username || blog.author}</li>
+    </ul>
   );
 };
 
