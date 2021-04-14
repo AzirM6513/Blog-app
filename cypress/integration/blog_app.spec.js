4a2dd854f35c8518700991a13ac402b449935982
azirm6513 <christianjortiz7+git@gmail.com>
2021-04-14 16:56:29 -0400

    add cypress/integration tests to test login

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -18,4 +18,27 @@ describe('Blog app', function () {
     cy.contains('password');
     cy.get('button').contains('login');
   });
+
+  describe('login', function () {
+    it('succeeds with correct information', function () {
+      cy.get('input:first').type('christian');
+      cy.get('input:last').type('cypress testing');
+      cy.get('button').contains('login').click();
+
+      cy.contains('Christian Ortiz logged-in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('input:first').type('username');
+      cy.get('input:last').type('password');
+      cy.get('button').contains('login').click();
+
+      cy.get('.message')
+        .should('contain', 'Wrong Username or Password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid');
+
+      cy.get('html').should('not.contain', 'Christian Ortiz logged-in');
+    });
+  });
 });
